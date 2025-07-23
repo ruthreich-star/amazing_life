61082cdfcef7e5ed28a454ec88d231e53d705e43
ruthreich-star <reich.ruth@gmail.com>
2025-07-23 21:19:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import numpy as np
 import amazing
 
 st.set_page_config(page_title="🐼 Pandas Pet Explorer", layout="centered")
-st.title("🐼 The New Pandas Pet Explorer")
+st.title("🐼 The New Pandas Pet Explorer new")
 
 amazing.show_pets()
